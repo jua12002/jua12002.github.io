52142ed923cbf6e71903c9f4932fe8d35a993705
jua12002 <jua12002@byui.edu>
2021-03-06 23:42:04 -0600

    update

--- lesson9/js/towns.js
+++ lesson9/js/towns.js
@@ -5,76 +5,42 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        console.table(jsonObject); // temporary checking for valid response and data parsing
+        console.table(jsonObject);
         const towns = jsonObject['towns'];
 
         for (let i = 0; i < towns.length; i++) {
-            let card = document.createElement('section');
-            let titleDiv = document.createElement('div');
-            titleDiv.classList.add('card-title');
-            let bodyDiv = document.createElement('div');
-            bodyDiv.classList.add('card-body');
+            if (i == 1 || i == 5 || i == 6) {
+                let card = document.createElement('div');
+                let info = document.createElement('section');
+                let photo = document.createElement('figure');
+                let name = document.createElement('h2');
+                let motto = document.createElement('h3');
+                let date = document.createElement('p');
+                let people = document.createElement('p');
+                let rain = document.createElement('p');
+                let image = document.createElement('img');
 
-            /*let h2 = document.createElement('h2');
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-            card.appendChild(h2);
-            document.querySelector('div.cards').appendChild(card);*/
+                name.textContent = towns[i].name;
+                motto.textContent = towns[i].motto;
+                date.textContent = 'Year Founded: ' + towns[i].yearFounded;
+                people.textContent = 'Population: ' + towns[i].currentPopulation;
+                rain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+                image.setAttribute('src', 'images/'+towns[i].photo);
+                image.setAttribute('alt', towns[i].name + ' Hometown');
 
-            // h1 - name
-            let townName = document.createElement('h1');
-            townName.textContent = towns[i].name;
-            titleDiv.appendChild(townName);
+                
+                photo.appendChild(image);
+                card.appendChild(photo);
 
-            // p - motto
-            let motto = document.createElement('p');
-            motto.textContent = ' ' + towns[i].motto;
-            bodyDiv.appendChild(motto);
+                info.appendChild(name);
+                info.appendChild(motto);
+                info.appendChild(date);
+                info.appendChild(people);
+                info.appendChild(rain);
+                card.appendChild(info);
 
-            // p - yearFounded
-            let yearFounded = document.createElement('p');
-            yearFounded.textContent = 'yearFounded' + towns[i].yearFounded;
-            bodyDiv.appendChild(yearFounded);
-
-            // p - currentPopulation
-            let currentPopulation = document.createElement('p');
-            currentPopulation.textContent = 'currentPopulation' + towns[i].currentPopulation;
-            bodyDiv.appendChild(currentPopulation);
-
-            // p - averageRainfall
-            let averageRainfall = document.createElement('p');
-            averageRainfall.textContent = 'averageRainfall' + towns[i].averageRainfall;
-            bodyDiv.appendChild(averageRainfall);
- 
-            // img - picture
-            let image = document.createElement('img');
-            image.setAttribute('src', prophets[i].imageurl);
-            image.setAttribute('loading', 'lazy');
-            image.setAttribute('alt', prophets[i].name + ' ' + towns[i].lastname + ' - ' + (i + 1));
-         
-            // p - events
-            let events = document.createElement('p');
-            events.textContent = 'events' + towns[i].events;
-            bodyDiv.appendChild(events);
- 
-            card.appendChild(titleDiv);
-            card.appendChild(image);
-            card.appendChild(bodyDiv);
-
-            document.querySelector('div.cards').appendChild(card);
+                document.getElementById('cards').appendChild(card);
+            }
         }
+
     });
-
-const imgHover = document.getElementsByTagName('img')[0];
-const sect = document.getElementsByTagName('section')[0];
-
-/*window.addEventListener('load',function () {
-    sect.classList.remove('loading');
-    
-});
-
-sect.addEventListener('mouseover', function() {
-    imgHover.classList.add('white-border');    
-},false)
-sect.addEventListener('mouseout',function () {
-    imgHover.classList.remove('white-border')    
-},false)*/
