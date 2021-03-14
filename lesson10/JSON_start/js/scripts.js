521d0f101ad8bff3a5fd375595d6e651c90ee1bb
jua12002 <jua12002@byui.edu>
2021-03-13 22:04:17 -0600

    update

--- lesson10/JSON_start/js/scripts.js
+++ lesson10/JSON_start/js/scripts.js
@@ -1,12 +1,12 @@
 //ADD the key and change units to imperial
-const apiURL = ""
+const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5604473&appid=79ecb9ae3225480c66f41a9ccbe4878b&units=imperial";
 
 //Go fetch it and then wait for a response.
 fetch(apiURL)
   .then((response) => response.json())
-  .then((changeme) => {
+  .then((weatherInfo) => {
     //Once it comes back, display it to the console.
-    console.log(changeme);
+    console.log(weatherInfo);
     
     
 
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,9 +1,9 @@
-const apiURL='https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=79ecb9ae3225480c66f41a9ccbe4878b&units=imperial';
+const apiURL='//api.openweathermap.org/data/2.5/weather?id=5604473&appid=79ecb9ae3225480c66f41a9ccbe4878b&units=imperial';
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
     console.log(jsObject);
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + 'png';
+    const imagesrc = '//openweathermap.org/img/w/' + jsObject.weather[0].icon + 'png';
     const desc = jsObject.weather[0].description; //weather array
     document.getElementById('imagesrc').textContent = imagesrc; //info specification
     document.getElementById('icon').setAttribute('src',imagesrc); //focus on the setAttribute
