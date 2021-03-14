7774bb2bd1a8fd5832e5c360c57ae3c3df8b431e
jua12002 <jua12002@byui.edu>
2021-03-13 23:22:13 -0600

    update

--- lesson10/JSON_start/js/scripts.js
+++ lesson10/JSON_start/js/scripts.js
@@ -19,6 +19,22 @@ fetch(apiURL)
 
     document.getElementById('weather_icon').src = icon_path;
 
+    //What Day is Today
+    const mydate = new Date();
+    const y = mydate.getDay();
+    console.log(y);
+
+    const myweekday = new Array(7);
+    myweekday[0]="Sunday";
+    myweekday[1]="Monday";
+    myweekday[2]="Tuesday";
+    myweekday[3]="Wednesday";
+    myweekday[4]="Thursday";
+    myweekday[0]="Friday";
+    myweekday[0]="Saturday";
+
+    //console.log(myweekday[3]);
+
  }); //end of "then" fat arrow function
 
 
