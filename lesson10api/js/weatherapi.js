80241c91409155afb2af3b2e0a2c4af6dfb715dc
jua12002 <jua12002@byui.edu>
2021-03-15 14:32:59 -0500

    update

--- lesson10api/js/weatherapi.js
+++ lesson10api/js/weatherapi.js
@@ -89,22 +89,22 @@ fetch(apiURL)
    
  }); //end of today weather
 
- // 5 days weather
-
-const forecastURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=79ecb9ae3225480c66f41a9ccbe4878b&units=imperial";
+// 5 days weather
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=79ecb9ae3225480c66f41a9ccbe4878b&units=imperial";
 fetch(forecastURL)
   .then((response) => response.json())
   .then((forecastObject) => {
-
     console.log(forecastObject)
     var forecast = forecastObject.list.filter(x => x.dt_txt.includes('18:00:00'));
     console.log(forecast)
+
     const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     
   for (let day = 0; day < forecast.length; day++) {
       const d = new Date(forecast[day].dt_txt);
       const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
       const desc = forecast[day].weather[0].description;
+
       document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
       document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast[day].main.temp);
       document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
