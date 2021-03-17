5e887e9b7b42b55afdddb18b9668df19d53d275b
jua12002 <jua12002@byui.edu>
2021-03-16 22:47:59 -0500

    update

--- sodaspring/js/weatherapi.js
+++ sodaspring/js/weatherapi.js
@@ -1,31 +1,7 @@
 //ADD the key and change units to imperial
-/*
-//Go fetch it and then wait for a response.
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((weatherInfo) => {
-    //Once it comes back, display it to the console.
-    console.log(weatherInfo);
 
-    document.getElementById('place').innerHTML=weatherInfo.name;
-    document.getElementById('currentTemp').innerHTML=weatherInfo.main.temp;
-    document.getElementById('windSpeed').innerHTML=weatherInfo.wind.speed;
-
-    document.getElementById('townName').innerHTML=weatherInfo.name;
-
-    const iconcode = weatherInfo.weather[0].icon;
-    console.log(iconcode);
-    const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
-    console.log(icon_path);
-
-    document.getElementById('weather_icon').src = icon_path;
-
-
-//console.log(myweekday[3]);
-*/
-
-//ADD the key and change units to imperial
-const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5604473&appid=79ecb9ae3225480c66f41a9ccbe4878b&units=imperial";
+// sodasprings ADD the key and change units to imperial
+const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5607916&appid=79ecb9ae3225480c66f41a9ccbe4878b&units=imperial";
 
 //Go fetch it and then wait for a response.
 fetch(apiURL)
@@ -90,7 +66,7 @@ fetch(apiURL)
  }); //end of today weather
 
 // 5 days weather
-const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=79ecb9ae3225480c66f41a9ccbe4878b&units=imperial";
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&appid=79ecb9ae3225480c66f41a9ccbe4878b&units=imperial";
 fetch(forecastURL)
   .then((response) => response.json())
   .then((forecastObject) => {
